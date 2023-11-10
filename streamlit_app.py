33518e1f94949f6e799b08dafddf5208630c7375
Lacerdash <fernandolacerdagrandini@gmail.com>
2023-11-09 19:27:31 -0500

    Removing unused libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import pandas as pd
 import streamlit as st
 import joblib
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as   go
 
